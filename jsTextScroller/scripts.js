2114ae1e4f7d829b8256b30bd0ee616e262512f8
Alan Callaghan <recodeexistance@gmail.com>
2019-07-10 02:59:51 +0100

    Create Battleship game folder.  Tidy other files.

--- jsTextScroller/scripts.js
+++
@@ -1,4 +0,0 @@
-function check() {
-	let grabit = document.getElementById("text-box"); 
-	document.write(grabit.value);
-}
---
+++ practiceCodeBlocks/validate.js
@@ -0,0 +1,9 @@
+let userName = {
+	fName: function() {
+		let firstName = document.getElementById("fName").value;
+		console.log(firstName);
+	}
+};
+
+
+//Screw it, going to have to go research how to do this then come back.  Work in progress. 
