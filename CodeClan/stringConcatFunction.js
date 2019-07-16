e43bec22beecad7eb7f4060ae9e76b600c3cc6b9
Alan Callaghan <recodeexistance@gmail.com>
2019-07-16 12:33:47 +0100

    one liner added here.  comments tidy.  functions as expected.

--- CodeClan/stringConcatFunction.js
+++ CodeClan/stringConcatFunction.js
@@ -1,26 +1,23 @@
-
 // Program for Admissions. 
-
 const getWeather = function(country, weatherType) {
 
-  //Assign the datatypes of our arguments to variables.  
-let countryDataType = typeof(country); 
-let weatherDataType = typeof(weatherType);
+    //Assign the datatypes of our arguments to variables.  
+    let countryDataType = typeof(country);
+    let weatherDataType = typeof(weatherType);
 
-  //Conditional to ensure datatypes are what we expect.
-if(countryDataType === "string" && weatherDataType === "string") {
-return (`The weather in ${country} is ${weatherType}.`);
-  } else {  // error thrown if incorrect datatypes.
-      /*
-        This is the code I'd use to return an error if the arguments passed to the parameters are not of the data type I want them to be.  
-      throw new TypeError(`Expected a string, instead recieved a ${countryDataType} and ${weatherDataType}`, "index.js", lookup callstack for line number here);
+    //Conditional to ensure datatypes are what we expect.
+    if (countryDataType === "string" && weatherDataType === "string") {
+        return (`The weather in ${country} is ${weatherType}.`);
+    } else {
+        /*  This is the code I'd use to return an error if the arguments passed to the parameters are not of the 
+            data type I want them to be.  
+        throw new TypeError(`Expected a string, instead recieved a ${countryDataType} and ${weatherDataType}`, "index.js", 
+        lookup callstack for line number here);  */
 
-      //But for the purpose of this exercise, here is a simple string result to inform us it's not valid.  */
-    return ("Incorrect value passed to function.");
-  }
+        //But for the purpose of this exercise, here is a simple string result to inform us it's not valid.  */
+        return ("Incorrect value passed to function.");
+    }
 };
-
-
 
 
 //tests run as expected.  
@@ -28,5 +25,11 @@ console.log(getWeather("Scotland", "sunny"));
 console.log(getWeather("Spain", "glorious"));
 console.log(getWeather("Thailand", "cooking"));
 
-  //test written to check datatype sanitizing.
+//test written to check datatype sanitizing.
 console.log(getWeather(50, "fail"));
+
+
+/* An alternate version of this code could be (note, i would keep the data sanitising as a function of it's 
+    own that I could call on the getWeather function externally, then pass it back):  
+
+const getWeather = (country, getWeather) => console.log(`The weather in ${country} is ${getWeather}`); 
