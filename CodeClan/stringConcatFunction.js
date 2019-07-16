7ca8b9c0d67a32f6e4bfe4b38520befefbf83fed
Alan Callaghan <recodeexistance@gmail.com>
2019-07-16 12:41:34 +0100

    Date added.  Merge starts here.

--- CodeClan/stringConcatFunction.js
+++ CodeClan/stringConcatFunction.js
@@ -1,4 +1,4 @@
-// Program for Admissions. 
+// Program for Admissions. Edited 16/07/2019
 const getWeather = function(country, weatherType) {
 
     //Assign the datatypes of our arguments to variables.  
@@ -11,8 +11,9 @@ const getWeather = function(country, weatherType) {
     } else {
         /*  This is the code I'd use to return an error if the arguments passed to the parameters are not of the 
             data type I want them to be.  
-        throw new TypeError(`Expected a string, instead recieved a ${countryDataType} and ${weatherDataType}`, "index.js", 
-        lookup callstack for line number here);  */
+            throw new TypeError(`Expected a string, instead recieved a ${countryDataType} and ${weatherDataType}`, "index.js", 
+            lookup callstack
+            for line number here);  */
 
         //But for the purpose of this exercise, here is a simple string result to inform us it's not valid.  */
         return ("Incorrect value passed to function.");
